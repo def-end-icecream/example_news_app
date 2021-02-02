32b800082af7bab9f1424b51cfeaebb33b755e81
dani <dani.zaghian@gmail.com>
2021-02-02 10:42:00 -0800

    adds api key credentials -- secured

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,6 +1,6 @@
 class Api::ArticlesController < ApplicationController
   def index
-    @articles = HTTP.headers("X-Api-Key" => "YOUR-API-KEY").get("http://newsapi.org/v2/everything?q=#{params[:search_terms]}").parse["articles"]
+    @articles = HTTP.headers("X-Api-Key" => Rails.application.credentials.news_api[:api_key]).get("http://newsapi.org/v2/everything?q=#{params[:search_terms]}").parse["articles"]
     render "index.json.jb"
   end
 end
