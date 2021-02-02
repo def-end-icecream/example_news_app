281260be91e58e0110a056c5d2a089ca413abe2d
dani <dani.zaghian@gmail.com>
2021-02-02 09:27:56 -0800

    adds http headers

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,6 +1,6 @@
 class Api::ArticlesController < ApplicationController
   def index
-    @articles = HTTP.get("http://newsapi.org/v2/everything?q=#{params[:search_terms]}&apiKey=YOUR-API-KEY-HERE").parse["articles"]
+    @articles = HTTP.headers("X-Api-Key" => "YOUR-API-KEY").get("http://newsapi.org/v2/everything?q=#{params[:search_terms]}").parse["articles"]
     render "index.json.jb"
   end
 end
